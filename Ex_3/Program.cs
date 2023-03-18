dd7c1a6b85a6600399ce8fefd04e1fe4506a661d
IlyaTom <ilytoma@mail.ru>
2023-03-18 15:09:11 +0300

    Work3

---
+++ Ex_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. (числа берете любые)
+
+int[] FillMass()
+{
+    int[] array = new int[8];
+    for (int i =0;i<8;i++)
+    {
+        array[i]=new Random().Next(100);
+    }
+    return array;
+}
+int[] array = FillMass();
+System.Console.WriteLine($"Полученный массив: [{String.Join(", ",array)}]");
